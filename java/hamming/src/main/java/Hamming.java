ea8f4540932c639f6e1e884da6423bc4f3e1f993
a.sallai <ndrs.sallai@gmail.com>
2017-07-01 20:37:24 +0200

    solve java/hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,28 @@
 public class Hamming {
 
+    private String leftStrand;
+
+    private String rightStrand;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException();
+        }
+
+        this.leftStrand = leftStrand;
+        this.rightStrand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        int distance = 0;
+
+        for (int i = 0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) {
+                distance++;
+            }
+        }
+
+        return distance;
     }
 
 }
